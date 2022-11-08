8d4bb0c2a5c68cd4cb0a0232d1af30e0b666a43a
Filip Wajgner <fwajgner@inovatica.com>
2022-11-08 13:48:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## ŁDI Inovatica - reużywalność komponentów
+
+![preview](./public/assets/preview.jpg)
+
+## Uruchomienie
+
+- Wymagane `node 16` i `npm 8` lub nowsze
+- Instalacja zależności: `npm ci`
+- Uruchomienie projektu: `npm run start` -> [localhost:3000](http://localhost:3000)
+
+## Licencja
+
+Licencja znajduje się w tym miejscu -> [LICENCJA](./LICENSE.md).
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+
+//
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+// ----------------------------------------------------------------------
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <HelmetProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </HelmetProvider>
+);
+
+// If you want to enable client cache, register instead.
+serviceWorker.unregister();
