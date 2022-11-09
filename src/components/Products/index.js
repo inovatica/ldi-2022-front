0270114bbce3e2599265cf53dbd5a994ababc058
Filip Wajgner <fwajgner@inovatica.com>
2022-11-09 23:23:10 +0100

    Remove product sort and filters

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,5 +1,3 @@
 export { ProductCard } from './ProductCard';
 export { ProductList } from './ProductList';
-export { ProductSort } from './ProductSort';
 export { ProductCartWidget } from './ProductCartWidget';
-export { ProductFilterSidebar } from './ProductFilterSidebar';
