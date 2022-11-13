a42c8dc18db462e8583b4ecc2b9d1f218a3ee1ea
Krystian Bendinger <krystian.bendinger@gmail.com>
2022-11-13 22:54:03 +0100

    add cart item list

--- src/components/Cart/CartItemList.js
+++ src/components/Cart/CartItemList.js
@@ -2,16 +2,16 @@ import PropTypes from 'prop-types';
 // @mui
 import { Grid } from '@mui/material';
 // components
-import { ProductCard } from './ProductCard';
+import { ProductCard } from '../Products/ProductCard';
 
 // ----------------------------------------------------------------------
 
-export const ProductList = (props) => {
-  const { products, ...other } = props;
+export const CartItemList = (props) => {
+  const { items, ...other } = props;
 
   return (
     <Grid container spacing={3} {...other}>
-      {products.map((product) => (
+      {items.map((product) => (
         <Grid key={product.id} item xs={12} sm={6} md={3}>
           <ProductCard product={product} />
         </Grid>
@@ -20,6 +20,6 @@ export const ProductList = (props) => {
   );
 };
 
-ProductList.propTypes = {
-  products: PropTypes.array.isRequired,
+CartItemList.propTypes = {
+  items: PropTypes.array.isRequired,
 };
--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,18 +1,22 @@
 // @mui
 import { Container, Typography } from '@mui/material';
-// components
 
-// mock
-// import PRODUCTS from '../_mock/products';
+import { useCartState } from '../providers/Cart';
+// components
+import { CartItemList } from '../components/Cart';
 
 // ----------------------------------------------------------------------
 
 const CartPage = () => {
+  const { items } = useCartState();
+
   return (
     <Container>
       <Typography variant='h4' sx={{ mb: 5 }}>
         Cart
       </Typography>
+
+      <CartItemList items={items} />
     </Container>
   );
 };
